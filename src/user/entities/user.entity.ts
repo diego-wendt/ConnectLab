b1df47eaee8c8c78e524c5f88a108a3f046a33b7
diego-wendt <diego_wendt@hotmail.com>
2022-12-23 00:09:31 -0300

    userEntity - ORM

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,38 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity({ name: 'user' })
 export class UserEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   name: string;
+
+  @Column()
+  url: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  passwords: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
 }
