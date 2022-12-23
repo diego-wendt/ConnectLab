4f92adf4a18e26ec02606e99a5d05ef6df80aabe
diego-wendt <diego_wendt@hotmail.com>
2022-12-23 00:28:10 -0300

    CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,52 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  IsStrongPassword,
+  IsUrl,
+  MaxLength,
+  MinLength,
+  ValidateIf,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(50)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @MinLength(3)
+  @MaxLength(50)
+  email: string;
+
+  //   @Column({ default: 'https://publicdomainvectors.org/photos/1389952697.png' })
+  @ValidateIf((dto) => dto.url !== '')
+  @MinLength(3)
+  @IsUrl()
+  url: string;
+
+  @ValidateIf((dto) => dto.phone !== '')
+  @IsNumberString()
+  @MinLength(10)
+  @MaxLength(11)
+  phone: string;
+
+  address: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+    minUppercase: 1,
+  })
+  password: string;
+
+  @IsNotEmpty()
+  password2: string;
+}
